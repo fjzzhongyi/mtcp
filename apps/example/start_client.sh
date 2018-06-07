a0327a5e9cf85f842005fe219dadade6516637f3
fjzzhongyi <fjzzhongyi@163.com>
2018-06-07 02:56:57 -0700

    update mtcp app client side

--- apps/example/start_client.sh
+++ apps/example/start_client.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-./epwget 192.168.1.10/example.txt 100 -n 4 -c 1000 -f epwget-multiprocess.conf
+./epwget 192.168.10.30/example.txt 10000 -n 4 -c 1000 -f epwget-multiprocess.conf
 for i in {5..7}
 do
-./epwget 192.168.1.10/example.txt 100 -n $i -c 1000 -f epwget-multiprocess.conf
+./epwget 192.168.10.30/example.txt 100 -n $i -c 1000 -f epwget-multiprocess.conf
 done
